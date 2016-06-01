13e020c80a152f2df364ad6baa44e2ca813ea45c
Taka Wang <wang.chia.ming@gmail.com>
2016-06-01 18:51:05 +0800

    implement init modbus tcp context

--- modbusd.c
+++ modbusd.c
@@ -17,23 +17,43 @@
 #define VERSION "0.0.1"
 #define IPC_SUB "ipc:///tmp/to.modbus"
 #define IPC_PUB "ipc:///tmp/from.modbus"
-#define DEFAULT_TCP_PORT 502
+#define MBTCP_RESP_TIMEOUT_SEC 3
+//#define DEFAULT_TCP_PORT 502
 
+// hashtable
+hash_mbtcp_t *mbtcp_htable = NULL;
 
 int init_mbtcp_ctx(const char *ip, int port)
 {
     modbus_t *ctx;
-    ctx = modbus_new_tcp("127.0.0.1", 1502);
-    if (ctx == NULL) {
-        fprintf(stderr, "Unable to allocate libmodbus context\n");
+    ctx = modbus_new_tcp(ip, port);
+    if (ctx == NULL)
+    {
+        fprintf(stderr, "Unable to allocate mbtcp context\n");
         return -1;
     }
-
+    
+    // set response timeout
+    modbus_set_response_timeout(ctx, MBTCP_RESP_TIMEOUT_SEC, 0);
+    
+    // add ctx to mbtcp hashtable
+    hash_mbtcp_t *mb_handler;
+    mb_handler = (hash_mbtcp_t*)malloc(sizeof(hash_mbtcp_t));
+    memset(mb_handler, 0, sizeof(mb_handler));
+    mb_handler->connected = false;
+    mb_handler->key.ip   = ip;
+    mb_handler->key.port = port;
+    mb_handler->ctx = ctx;
+    HASH_ADD(hh, mbtcp_htable, key, sizeof(key_mbtcp_t), mb_handler);
+    printf("Add %s:%n to hashtable\n", mb_handler->key.ip, mb_handler->key.port);
+    return 0;
+/*
     if (modbus_connect(ctx) == -1) {
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
         modbus_free(ctx);
         return -1;
     }
+*/
 }
 
 void get_mbtcp_ctx()
