bc524596e27c8fd033d3943d9e85601c26eb20a6
Taka Wang <wang.chia.ming@gmail.com>
2016-06-06 15:25:40 +0800

    add unit16 support

--- json/cJSON.h
+++ json/cJSON.h
@@ -104,7 +104,10 @@ extern cJSON *cJSON_CreateArray(void);
 extern cJSON *cJSON_CreateObject(void);
 
 /* These utilities create an Array of count items. */
-extern cJSON *cJSON_CreateUInt8Array(const uint8_t *numbers,int count); // taka add
+ // taka add
+extern cJSON *cJSON_CreateUInt8Array(const uint8_t *numbers,int count);
+extern cJSON *cJSON_CreateUInt16Array(const uint8_t *numbers,int count);
+
 extern cJSON *cJSON_CreateIntArray(const int *numbers,int count);
 extern cJSON *cJSON_CreateFloatArray(const float *numbers,int count);
 extern cJSON *cJSON_CreateDoubleArray(const double *numbers,int count);
