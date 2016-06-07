1d51ac78a18bd186a6268e06e4855dc1c7b4f678
Taka Wang <wang.chia.ming@gmail.com>
2016-06-07 14:02:09 +0800

    add test server

---
+++ tests/mbserver/server.js
@@ -0,0 +1,35 @@
+var net = require('net');
+var assert  = require("assert");
+var helpers = require("./helpers");
+var trials  = helpers.trials();
+var coils   = helpers.randomBits();
+
+var mserver = net.createServer(function (socket) {
+    var server = new modbus.Server();
+	server.writer().pipe(socket);
+	socket.pipe(server.reader());
+    
+    // FC1
+    server.on("read-coils", function (from, to, reply) {
+        return reply(null, coils.slice(from, to + 1));
+    });
+
+    describe("READ_COILS", function () {
+        for (var i = 0; i < trials; i++) {
+            (function () {
+                var from = Math.max(0, Math.floor(Math.random() * coils.length / 2));
+                var to   = Math.min(coils.length, from + Math.floor(Math.random() * coils.length / 2));
+
+                it("should pass (" + from + "-" + to + ")", function () {
+                    return run(from, to);
+                });
+            })();
+        }
+    });
+    
+    
+}
+
+mserver.listen(502, function() {
+	console.log('listening...');
+});
