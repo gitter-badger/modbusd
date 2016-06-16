653d68f12110160ecb24e747c075e96a41420a95
Taka Wang <wang.chia.ming@gmail.com>
2016-06-16 15:34:29 +0800

    pass version string from cmake

--- main.h
+++ main.h
@@ -10,5 +10,8 @@
  *  marco
 ================================================== */
 //#define VERSION "0.2.1"
+#ifndef VERSION_LIBINTERFACE
+    #  define VERSION_LIBINTERFACE @MB_VERSION@
+#endif
 
 #endif  // MAIN_H
