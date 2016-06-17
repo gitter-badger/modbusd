75804dc6e870ee458f201701186a2b67e620eb9f
Taka Wang <wang.chia.ming@gmail.com>
2016-06-17 10:12:32 +0800

    update typo

--- tests/goclient/client.go
+++ tests/goclient/client.go
@@ -61,6 +61,6 @@ func subscriber() {
 	filter := ""
 	receiver.SetSubscribe(filter) // filter frame 1
 	msg, _ := receiver.RecvMessage(0)
-	fmt.Println(msg[0])  // frame 1: method
-	fmt.Println(command) // frame 2: command
+	fmt.Println(msg[0]) // frame 1: method
+	fmt.Println(msg[1]) // frame 2: command
 }
